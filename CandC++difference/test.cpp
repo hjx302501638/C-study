b1acda3ac9735466b6c0d0c8ef0fe498b6cba56f
HJX302501638 <302501638@qq.com>
2019-09-20 00:11:04 +0800

    测试C与C++不同之处，变量定义

---
+++ CandC++difference/test.cpp
@@ -0,0 +1,27 @@
+
+/*  ±‡“Îª∑æ≥
+	gcc version 5.4.0 20160609  
+*/
+#include <stdio.h>
+
+//int global_val ;
+//int global_val ;
+
+int main(int argc, char *argv[])
+{
+	printf("Begin...\n");
+    int val=0;
+    for(int i=1; i <10; i++)
+	{
+		for(int j=1;i<10; j++)
+		{
+			
+			val = i+j;
+		}	
+    }
+    printf("val = %d\n",val);
+    printf("End...\n");
+    
+    return 0;
+}
+
