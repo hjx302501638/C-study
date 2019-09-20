b8bb87b8f3e1b0e7337da6b4101c009146b7ff0b
HJX302501638 <302501638@qq.com>
2019-09-20 21:04:07 +0800

    candc++difference test ok

--- CandC++difference/test.cpp
+++ CandC++difference/test.cpp
@@ -6,19 +6,45 @@
 
 //int global_val ;
 //int global_val ;
-
+struct test_struct 
+{
+	int a;
+	int b;
+	int c;
+};
+int f(void)
+{
+	return 10;
+}
+int f1()
+{
+	return 11;
+}
 int main(int argc, char *argv[])
 {
+	test_struct S1 = {1,2,3};
 	printf("Begin...\n");
     int val=0;
     for(int i=1; i <10; i++)
 	{
-		for(int j=1;i<10; j++)
+		for(int j=1;j<10; j++)
 		{
 			
 			val = i+j;
 		}	
     }
+ 	int *p;
+	p=&S1.a;
+	for(int i=0;i <3 ;i++)
+	printf("test_struct val_%d = %d\n",i,*p++);
+	
+	printf("f(void)= %d\n",f());
+	printf("f(void)= %d\n",f1());
+	const int const_val = 0;
+	p = (int *)&const_val;
+	*p = 10;
+    	printf("const_val = %d\n",const_val);	
+    	printf("val = %d\n",val);
     printf("val = %d\n",val);
     printf("End...\n");
     
